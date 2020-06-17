df5db6ce5e2307ed0185c5ecf7959ea194871b67
STYZY <styzyreal@126.com>
2020-06-17 10:33:57 +0800

    v1.5.2更新：awesome模式新增HTMLElement原型insertAfter方法

---
+++ src/classesExtendMethods/HTMLElement.js
@@ -0,0 +1,16 @@
+const methods = {
+    insertAfter
+}
+
+function insertAfter(newElement, targetElement) {
+    if (targetElement.parentNode !== this) {
+        throw new DOMException(`Failed to execute 'insertAfter' on 'Node': The node after which the new node is to be inserted is not a child of this node.`)
+    }
+    if (this.lastChild === targetElement) {
+        this.appendChild(newElement)
+    } else {
+        this.insertBefore(newElement, targetElement.nextSibling)
+    }
+}
+
+export default methods
--- src/classesExtendMethods/index.js
+++ src/classesExtendMethods/index.js
@@ -1,9 +1,11 @@
 import DateMethods from './Date'
 import StringMethods from './String'
+import HTMLElementMethods from './HTMLElement'
 
 const classes = {
-    'String': StringMethods,
-    'Date': DateMethods
+    String: StringMethods,
+    Date: DateMethods,
+    HTMLElement: HTMLElementMethods
 }
 
 export default classes
