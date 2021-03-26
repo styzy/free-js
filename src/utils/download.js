6b4a316392d2315548de3206727baee17548cae4
STYZY <styzyreal@126.com>
2021-03-26 15:17:46 +0800

    v1.8.1更新：utils模块download方法新增下载成功回调，接收参数fileName

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,31 +1,52 @@
-const download = function (url, fileName = getFileName(url)) {
-    if (checkOrigin(url)) {
-        // 同域
-        let el = document.createElement('a')
-        el.setAttribute('download', fileName)
-        el.href = url
-        el.click()
-    } else {
-        // 跨域
-        crossOriginDownload(url, fileName)
+const download = function (url, { fileName = getFileName(url), headers = {}, onSuccess = () => {} } = {}) {
+    try {
+        if (checkOrigin(url)) {
+            // 同域
+            let el = document.createElement('a')
+            el.setAttribute('download', fileName)
+            el.href = url
+            el.click()
+            onSuccess(fileName)
+        } else {
+            // 跨域
+            crossOriginDownload()
+        }
+    } catch (error) {
+        console.error('free download error:\n', error)
     }
-}
 
-function crossOriginDownload(url, fileName) {
-    let xhr = new XMLHttpRequest()
-    if (!xhr) {
-        throw new Error('当前浏览器不支持XMLHttpRequest，无法下载')
+    function crossOriginDownload() {
+        let xhr = null
+        try {
+            xhr = new XMLHttpRequest()
+        } catch (error) {
+            throw new Error('当前浏览器不支持XMLHttpRequest，无法下载')
+        }
+
+        xhr.open('GET', url, true)
+        xhr.responseType = 'blob'
+
+        for (const headerName in headers) {
+            if (Object.hasOwnProperty.call(headers, headerName)) {
+                xhr.setRequestHeader(headerName, headers[headerName])
+            }
+        }
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                let url = window.URL.createObjectURL(xhr.response),
+                    el = document.createElement('a')
+                el.href = url
+                el.download = fileName
+                el.click()
+                onSuccess(fileName)
+            }
+        }
+
+        try {
+            xhr.send()
+        } catch (error) {}
     }
-    xhr.open('GET', url, true)
-    xhr.responseType = 'blob'
-    xhr.onload = function (e) {
-        let url = window.URL.createObjectURL(xhr.response),
-            el = document.createElement('a')
-        el.href = url
-        el.download = fileName
-        el.click()
-    }
-    xhr.send()
 }
 
 function checkOrigin(url) {
@@ -34,7 +55,7 @@ function checkOrigin(url) {
         let targetUrl = new URL(url)
         return localUrl.origin === targetUrl.origin
     } catch (error) {
-        throw new Error(`错误的下载地址${url}`)
+        throw new Error(`错误的下载地址:${url}`)
     }
 }
 
